c05f8e22b07f3bda8d73c44f29603a2bcc7f429c
Crlsocro <christian.r.lie@hotmail.no>
2021-09-06 10:03:04 +0200

    #2 part 02 done

--- user-collections/src/main/kotlin/org/tsdes/usercollections/RestApi.kt
+++ user-collections/src/main/kotlin/org/tsdes/usercollections/RestApi.kt
@@ -2,6 +2,7 @@ package org.tsdes.usercollections
 
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.tsdes.usercollections.db.UserService
@@ -10,8 +11,8 @@ import org.tsdes.usercollections.dto.UserDto
 
 @Api(value = "/api/usercollections", description = "Operations on card collections owned by users")
 @RequestMapping(
-        path = ["/api/user-collections"]
-                produces = [(MediaType.APPLICATION_JSON_VALUE)]
+        path = ["/api/user-collections"],
+        produces = [(MediaType.APPLICATION_JSON_VALUE)]
 )
 
 @RestController
