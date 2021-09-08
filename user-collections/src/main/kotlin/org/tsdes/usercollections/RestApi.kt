796d3214b762112dbfe793cda6a8cdc1efa7adf0
chris <christian.r.lie@hotmail.no>
2021-09-08 21:12:21 +0200

    Part 03 done. close #3

--- user-collections/src/main/kotlin/org/tsdes/usercollections/RestApi.kt
+++ user-collections/src/main/kotlin/org/tsdes/usercollections/RestApi.kt
@@ -6,39 +6,91 @@ import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.tsdes.usercollections.db.UserService
+import org.tsdes.usercollections.dto.Command
+import org.tsdes.usercollections.dto.PatchResultDto
+import org.tsdes.usercollections.dto.PatchUserDto
 import org.tsdes.usercollections.dto.UserDto
+import java.lang.IllegalArgumentException
 
 
-@Api(value = "/api/usercollections", description = "Operations on card collections owned by users")
+@Api(value = "/api/user-collections", description = "Operations on card collections owned by users")
 @RequestMapping(
-        path = ["/api/user-collections"],
-        produces = [(MediaType.APPLICATION_JSON_VALUE)]
+    path = ["/api/user-collections"],
+    produces = [(MediaType.APPLICATION_JSON_VALUE)]
 )
-
 @RestController
-class RestApi(
-        private val userService: UserService
+class RestAPI(
+    private val userService: UserService
 ) {
+
     @ApiOperation("Retrieve card collection information for a specific user")
-    @GetMapping(path = ["/{userId"])
+    @GetMapping(path = ["/{userId}"])
     fun getUserInfo(
-            @PathVariable("userId") userId: String
-    ): ResponseEntity<UserDto> {
+        @PathVariable("userId") userId: String
+    ) : ResponseEntity<UserDto>{
 
         val user = userService.findByIdEager(userId)
-        if (user == null) {
+        if(user == null){
             return ResponseEntity.notFound().build()
         }
+
         return ResponseEntity.status(200).body(DtoConverter.transform(user))
     }
 
     @ApiOperation("Create a new user, with the given id")
-    @PutMapping(path = ["/{userId"])
+    @PutMapping(path = ["/{userId}"])
     fun createUser(
-            @PathVariable("userId") userId: String
-    ) : ResponseEntity<Void>{
+        @PathVariable("userId") userId: String
+    ): ResponseEntity<Void>{
         val ok = userService.registerNewUser(userId)
-        return if(!ok) ResponseEntity.status(400).build()
-            else ResponseEntity.status(201).build()
+        return if(!ok)  ResponseEntity.status(400).build()
+        else ResponseEntity.status(201).build()
+    }
+
+    @ApiOperation("Execute a command on a user's collection, like for example buying/milling cards")
+    @PatchMapping(
+        path = ["/{userId}"],
+        consumes = [(MediaType.APPLICATION_JSON_VALUE)]
+    )
+    fun patchUser(
+        @PathVariable("userId") userId: String,
+        @RequestBody dto: PatchUserDto
+    ): ResponseEntity<PatchResultDto>{
+
+        if(dto.command == null){
+            return ResponseEntity.status(400).build()
+        }
+
+        if(dto.command == Command.OPEN_PACK){
+            val ids = try {
+                userService.openPack(userId)
+            } catch (e: IllegalArgumentException){
+                return ResponseEntity.status(400).build()
+            }
+            return ResponseEntity.status(200).body(PatchResultDto().apply { cardIdsInOpenedPack.addAll(ids) })
+        }
+
+        val cardId = dto.cardId
+            ?: return ResponseEntity.status(400).build()
+
+        if(dto.command == Command.BUY_CARD){
+            try{
+                userService.buyCard(userId, cardId)
+            } catch (e: IllegalArgumentException){
+                return ResponseEntity.status(400).build()
+            }
+            return ResponseEntity.status(200).body(PatchResultDto())
+        }
+
+        if(dto.command == Command.MILL_CARD){
+            try{
+                userService.millCard(userId, cardId)
+            } catch (e: IllegalArgumentException){
+                return ResponseEntity.status(400).build()
+            }
+            return ResponseEntity.status(200).body(PatchResultDto())
+        }
+
+        return ResponseEntity.status(400).build()
     }
 }
--- user-collections/src/test/kotlin/org/tsdes/usercollections/RestApiTest.kt
+++ user-collections/src/test/kotlin/org/tsdes/usercollections/RestApiTest.kt
@@ -2,6 +2,8 @@ package org.tsdes.usercollections
 
 import io.restassured.RestAssured
 import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -9,25 +11,43 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.context.annotation.Primary
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Service
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.tsdes.usercollections.db.UserRepository
 import org.tsdes.usercollections.db.UserService
+import org.tsdes.usercollections.dto.Command
+import org.tsdes.usercollections.dto.PatchUserDto
+import org.tsdes.usercollections.model.Collection
 import javax.annotation.PostConstruct
 
-@ActiveProfiles("test")
+@Profile("RestAPITest")
+@Primary
+@Service
+class FakeCardService : CardService() {
+    override fun fetchData() {
+        val dto = FakeData.getCollectionDto()
+        super.collection = Collection(dto)
+    }
+}
+
+
+@ActiveProfiles("RestAPITest","test")
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-internal class RestApiTest {
+internal class RestAPITest {
 
     @LocalServerPort
     protected var port = 0
 
     @Autowired
-    private lateinit var userService : UserService
+    private lateinit var userService: UserService
 
     @Autowired
     private lateinit var userRepository: UserRepository
+
 
     @PostConstruct
     fun init() {
@@ -45,24 +65,97 @@ internal class RestApiTest {
 
     @Test
     fun testGetUser(){
+
         val id = "foo"
         userService.registerNewUser(id)
 
         given().get("/$id")
-                .then()
-                .statusCode(201)
-
-        assertTrue(userRepository.existsById(id))
+            .then()
+            .statusCode(200)
     }
 
     @Test
-    fun testCreateUser(){
+    fun testCreateUser() {
         val id = "foo"
 
         given().put("/$id")
-                .then()
-                .statusCode(201)
+            .then()
+            .statusCode(201)
 
         assertTrue(userRepository.existsById(id))
     }
+    @Test
+    fun testBuyCard() {
+
+        val userId = "foo"
+        val cardId = "c00"
+
+        given().put("/$userId").then().statusCode(201)
+
+        given().contentType(ContentType.JSON)
+            .body(PatchUserDto(Command.BUY_CARD, cardId))
+            .patch("/$userId")
+            .then()
+            .statusCode(200)
+
+        val user = userService.findByIdEager(userId)!!
+        assertTrue(user.ownedCards.any { it.cardId == cardId })
+    }
+
+
+    @Test
+    fun testOpenPack() {
+
+        val userId = "foo"
+        given().auth().basic(userId, "123").put("/$userId").then().statusCode(201)
+
+        val before = userService.findByIdEager(userId)!!
+        val totCards = before.ownedCards.sumBy { it.numberOfCopies }
+        val totPacks = before.cardPacks
+        assertTrue(totPacks > 0)
+
+        given().contentType(ContentType.JSON)
+            .body(PatchUserDto(Command.OPEN_PACK))
+            .patch("/$userId")
+            .then()
+            .statusCode(200)
+
+        val after = userService.findByIdEager(userId)!!
+        Assertions.assertEquals(totPacks - 1, after.cardPacks)
+        Assertions.assertEquals(totCards + UserService.CARDS_PER_PACK,
+            after.ownedCards.sumBy { it.numberOfCopies })
+    }
+
+
+    @Test
+    fun testMillCard() {
+
+        val userId = "foo"
+        given().put("/$userId").then().statusCode(201)
+
+        val before = userRepository.findById(userId).get()
+        val coins = before.coins
+
+        given().contentType(ContentType.JSON)
+            .body(PatchUserDto(Command.OPEN_PACK))
+            .patch("/$userId")
+            .then()
+            .statusCode(200)
+
+        val between = userService.findByIdEager(userId)!!
+        val n = between.ownedCards.sumBy { it.numberOfCopies }
+
+
+        val cardId = between.ownedCards[0].cardId!!
+        given().contentType(ContentType.JSON)
+            .body(PatchUserDto(Command.MILL_CARD, cardId))
+            .patch("/$userId")
+            .then()
+            .statusCode(200)
+
+        val after = userService.findByIdEager(userId)!!
+        assertTrue(after.coins > coins)
+        Assertions.assertEquals(n - 1, after.ownedCards.sumBy { it.numberOfCopies })
+    }
+
 }
